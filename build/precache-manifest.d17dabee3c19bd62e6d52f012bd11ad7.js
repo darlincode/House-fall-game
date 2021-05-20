dce833a3071eccfce1da47c40d5d2cad032c40c8
nwannachukwuemekad@gmail.com <nwannachukwuemekad@gmail.com>
2021-05-20 09:13:21 +0100

    housefall-game

---
+++ build/precache-manifest.d17dabee3c19bd62e6d52f012bd11ad7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a023299f4b0fd65a52383ab060d010f",
+    "url": "/static/media/perlin.0a023299.png"
+  },
+  {
+    "revision": "b9ea13bdd2d73b5fd783c02d47201a3c",
+    "url": "/static/media/jet.b9ea13bd.glb"
+  },
+  {
+    "revision": "096ec86bf44273b6524c161d5cfc9aff",
+    "url": "/static/media/droid.096ec86b.fbx"
+  },
+  {
+    "revision": "de89fa670a5512c42c56112b0818490f",
+    "url": "/static/media/cuphead.de89fa67.png"
+  },
+  {
+    "revision": "8bb8f478403e61e3138c7bb42465b0f7",
+    "url": "/static/media/crash-01.8bb8f478.wav"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23067a595d8e3f71e931",
+    "url": "/static/js/main.bad9cdd1.chunk.js"
+  },
+  {
+    "revision": "e0f9f1b567c3ad9c7949",
+    "url": "/static/js/2.937c5211.chunk.js"
+  },
+  {
+    "revision": "23067a595d8e3f71e931",
+    "url": "/static/css/main.179a0f4e.chunk.css"
+  },
+  {
+    "revision": "ab1496590f4ea133e2f7a546f7ba7a06",
+    "url": "/index.html"
+  }
+];
